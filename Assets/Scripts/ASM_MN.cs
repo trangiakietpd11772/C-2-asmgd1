56b581569934f41810433b6d7e202f77961bf5ba
KietVjp <trangiakiet180204@gmail.com>
2025-06-03 16:34:02 +0700

    lan 1

---
+++ Assets/Scripts/ASM_MN.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System.IO;
+using System.Threading;
+using System;
+
+public class ASM_MN : Singleton<ASM_MN>
+{
+    public List<Region> listRegion = new List<Region>();
+    public List<Players> listPlayer = new List<Players>();
+
+    private void Start()
+    {
+        createRegion();
+    }
+
+    public void createRegion()
+    {
+        listRegion.Add(new Region(0, "VN"));
+        listRegion.Add(new Region(1, "VN1"));
+        listRegion.Add(new Region(2, "VN2"));
+        listRegion.Add(new Region(3, "JS"));
+        listRegion.Add(new Region(4, "VS"));
+    }
+
+    public string calculate_rank(int score)
+    {
+        // sinh viên viết tiếp code ở đây
+        if (score < 100)
+            return "Đồng";
+        else if (score < 500)
+            return "Bạc";
+        else if (score < 1000)
+            return "Vàng";
+        else
+            return "Kim cương";
+
+    }
+
+    public void YC1()
+    {
+        // sinh viên viết tiếp code ở đây
+        int id = ScoreKeeper.Instance.GetID();
+        string name = ScoreKeeper.Instance.GetUserName();
+        int Score = ScoreKeeper.Instance.GetScore();
+        int regionId = ScoreKeeper.Instance.GetIDregion();
+
+        Region region = listRegion.Find(r => r.ID == regionId);
+
+        Players newPlayer = new Players(id, name, Score, region);
+
+
+        listPlayer.Add(newPlayer);
+
+        Debug.Log($"Da luu: {name} - ID: {id} - score: {Score} - region: {region}");
+    }
+    public void YC2()
+    {
+        // sinh viên viết tiếp code ở đây
+
+        foreach (Players p in listPlayer)
+        {
+            string rank = calculate_rank(p.Score);
+            Debug.Log($"ID: {p.Id}, Name: {p.Name}, Score: {p.Score}, Region: {p.Region.Name}, Rank: {rank}");
+        }
+
+    }
+    public void YC3()
+    {
+        // sinh viên viết tiếp code ở đây
+    }
+    public void YC4()
+    {
+        // sinh viên viết tiếp code ở đây
+    }
+    public void YC5()
+    {
+        // sinh viên viết tiếp code ở đây
+    }
+    public void YC6()
+    {
+        // sinh viên viết tiếp code ở đây
+    }
+    public void YC7()
+    {
+        // sinh viên viết tiếp code ở đây
+    }
+    void CalculateAndSaveAverageScoreByRegion()
+    {
+        // sinh viên viết tiếp code ở đây
+    }
+
+}
+
+[SerializeField]
+public class Region
+{
+    public int ID;
+    public string Name;
+    public Region(int ID, string Name)
+    {
+        this.ID = ID;
+        this.Name = Name;
+    }
+}
+
+[SerializeField]
+public class Players
+{
+    // sinh viên viết tiếp code ở đây
+    public int Id; // Mã nhân vật
+    public string Name; // tên nhân vâtj
+    public int Score; // điểm số đạt đc
+    public Region Region; // vùng quốc gia
+    public Players(int id, string name, int score, Region region)
+    {
+        this.Id = id;
+        this.Name = name;
+        this.Score = score;
+        this.Region = region;
+    }
+}
---
+++ Assets/Scripts/UIGameOver.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIGameOver : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI scoreText;
+    ScoreKeeper scoreKeeper;
+    ASM_MN ASM_MN;
+
+    void Awake()
+    {
+        scoreKeeper = ScoreKeeper.Instance;
+        ASM_MN = ASM_MN.Instance;
+    }
+
+    void Start()
+    {
+        scoreText.text = "You Scored:\n" + scoreKeeper.GetScore();
+
+        ASM_MN.YC1();
+        ASM_MN.YC2();
+        ASM_MN.YC3();
+        ASM_MN.YC4();
+        ASM_MN.YC5();
+        ASM_MN.YC6();
+        ASM_MN.YC7();
+    }
+
+    
+
+
+}
